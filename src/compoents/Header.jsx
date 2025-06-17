c041aca2bc8da58551598cb7d56643cf4af11374
AkankshaGajageshwar <gajageshwarakanksha@gmail.com>
2025-06-17 19:12:15 +0530

    changes

--- src/compoents/Header.jsx
+++ src/compoents/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import '../assets/css/style.css';
-
+//changes
 export default function Header() {
 
 	const count = useSelector((state) => state.cart.products)
